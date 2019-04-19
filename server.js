73c5c8f571ca60d8818e2257b47e6f02344da992
babuchellathurai <babu.abps@gmail.com>
2019-04-19 12:21:37 -0400

    Add Template engine handlebars

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express'),
-app = express();
+app = express(),
+hbs = require('express-handlebars').create({ defaultLayout: 'index.hbs'});
 
 app.use(express.static('public'));
 
-app.get('/',(request,response)=>{
-  response.send('Hello Express!');
+//setting our app engine to handlebars
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+
+app.get('/',(request, response)=>{
+  // response.send('Hello Express!');
+
+  response.render('homepage', {title: 'main page'});
 });
 
 app.listen(3010,()=>console.log('node.js express server started at port 3010'));
